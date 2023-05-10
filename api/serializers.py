7fb03d5b0a33b49aa2b37f0f6fd9f46ebb10d9c6
micahfay <micah@nexleaf.org>
2023-05-10 04:56:43 -0400

    added some help text

--- api/serializers.py
+++ api/serializers.py
@@ -11,7 +11,7 @@ class ParkingLotSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100, required=True)
     spaces = serializers.ListField(child=serializers.ChoiceField(
         choices=SpaceTypes.choices),
-        help_text="Array of parking spots by type, 1 for a Motorcycle spot, "
+        help_text="Array of parking spots by type, 1 for a motorcycle spot, "
                   "2 for a car spot and 3 for a van spot", required=True)
 
     def save(self):
@@ -32,16 +32,23 @@ class ParkingLotSerializer(serializers.Serializer):
 
 
 class ParkingLotQuerySerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
+    name = serializers.CharField(max_length=100, help_text='the name of the '
+                                                           'parking lot')
 
 
 class ParkingLotStatusSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
-    full = serializers.SerializerMethodField()
-    motorcycle_spots_available = serializers.SerializerMethodField()
-    car_spots_available = serializers.SerializerMethodField()
-    van_spots_available = serializers.SerializerMethodField()
-    spots_taken_by_vans = serializers.SerializerMethodField()
+    name = serializers.CharField(max_length=100, help_text='the name of the '
+                                                           'parking lot')
+    full = serializers.SerializerMethodField(
+        help_text='returns True if the lot is full else False')
+    motorcycle_spots_available = serializers.SerializerMethodField(
+        help_text='returns the number of motorcycle spots still available')
+    car_spots_available = serializers.SerializerMethodField(
+        help_text='returns the number of car spots still available')
+    van_spots_available = serializers.SerializerMethodField(
+        help_text='returns the number of van spots still available')
+    spots_taken_by_vans = serializers.SerializerMethodField(
+        help_text='returns the number of spots currently occupied by vans')
 
     def get_full(self, obj):
         return not ParkingSpace.objects.filter(lot=obj, vehicle=None).exists()
@@ -75,9 +82,13 @@ class ParkingLotStatusSerializer(serializers.Serializer):
 
 
 class ParkVehicleSerializer(serializers.Serializer):
-    type = serializers.ChoiceField(choices=VehicleTypes.choices)
-    lot_name = serializers.CharField(max_length=100)
-    license_plate = serializers.CharField(max_length=10)
+    type = serializers.ChoiceField(
+        choices=VehicleTypes.choices,
+        help_text=' '.join([f'{vt.name}: {vt.value}' for vt in VehicleTypes]))
+    lot_name = serializers.CharField(
+        max_length=100, help_text='The name of the parking lot')
+    license_plate = serializers.CharField(
+        max_length=10, help_text='Vehicles license plate number')
 
     def validate(self, data):
         lot = generics.get_object_or_404(
@@ -144,8 +155,10 @@ class ParkVehicleSerializer(serializers.Serializer):
 
 
 class VehicleSerializer(serializers.Serializer):
-    license_plate = serializers.CharField(max_length=10)
-    minutes_parked = serializers.SerializerMethodField()
+    license_plate = serializers.CharField(
+        max_length=10, help_text='Vehicles License plate number')
+    minutes_parked = serializers.SerializerMethodField(
+        help_text='returns how long the vehicle has been parked')
 
     def get_minutes_parked(self, obj):
         duration = datetime.now(pytz.UTC) - obj.parked_at
